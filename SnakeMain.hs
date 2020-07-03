f146b5ce0cb4bed45a168de8c875657d789843d2
Jason Liu <m.jason.liu@outlook.com>
2020-07-03 18:33:33 +0800

    hello world

--- SnakeMain.hs
+++ SnakeMain.hs
@@ -1,3 +1,16 @@
+import Graphics.Gloss
 
-main = 
-    print "hello world"
+main :: IO ()
+main = display
+        (InWindow
+               "Hello World"     -- window title
+                (400, 150)       -- window size
+                (10, 10))        -- window position
+        green                    -- background color
+        picture                  -- picture to display
+
+picture :: Picture
+picture
+        = Translate (-170) (-20) -- shift the text to the middle of the window
+        $ Scale 0.5 0.5          -- display it half the original size
+        $ Text "Hello World"     -- text to display
